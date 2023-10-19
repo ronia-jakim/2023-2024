d45706d5c899ba61402ec33b68dae2d88066df43
maruda <weronikajakimowicz2@gmail.com>
2023-10-19 20:26:36 +0200

    czwartek-cierpien

---
+++ funkcyjne/pracownia/punkty.py
@@ -0,0 +1,32 @@
+max = [
+    [3, 1, 6, 1, 2, 2, 2, 3],
+    [2, 1, 1, 4, 2, 3, 4, 1, 2]
+    ]
+
+
+my_points =[
+    [3, 1, 6, 1, 2, 2, 2, 3],
+    [2, 0, 1, 4, 0, 3, 4, 1, 0]
+    ]
+
+sum_max = []
+MAX = 0
+
+for lst in max:
+    sum_max.append(sum(lst))
+MAX = sum(sum_max)
+
+sum_my = []
+MY = 0
+
+for lst in my_points:
+    sum_my.append(sum(lst))
+MY = sum(sum_my)
+
+print(" Dostałam " + str(MY) + "/" + str(MAX) + " = " + str(MY/MAX) + "%")
+print("========================")
+
+i = 0
+for lst in max:
+    print(" Lista " + str((i + 1)) + " :: " + str(sum_my[i]) + "/" + str(sum_max[i]) + " = " + str(sum_my[i]/sum_max[i]))
+    i += 1
